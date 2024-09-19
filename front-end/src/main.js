cdf1d789222eeaffe2d2b2d70f50c2de2427b38a
subinPark <subin4420@gmail.com>
2024-09-19 11:14:24 +0900

    add vuetify config

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
 import './styles/global.css';  // 전역 스타일 불러오기
+import vuetify from './plugins/vuetify'; // Vuetify 플러그인
 
 const app = createApp(App);
 
 app.use(router);
 app.use(createPinia());
-
+app.use(vuetify);
 app.mount('#app');
