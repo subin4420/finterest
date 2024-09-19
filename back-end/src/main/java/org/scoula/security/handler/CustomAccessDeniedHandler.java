4218122cc916667813dd321dd0e34e5f1e996bc9
subinPark <subin4420@gmail.com>
2024-09-19 11:13:43 +0900

    add backend spring legacy directory structure

---
+++ back-end/src/main/java/org/scoula/security/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,27 @@
+package org.scoula.security.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.scoula.security.util.JsonResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component
+@Slf4j
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.error("========== 인가 에러 ============");
+        JsonResponse.sendError(response, HttpStatus.FORBIDDEN, "권한이 부족합니다.");
+    }
+}
