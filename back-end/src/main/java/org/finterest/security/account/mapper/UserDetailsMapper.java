1aecad3c02b205381a60dfd82752ae6314e0c9cb
subinPark <subin4420@gmail.com>
2024-09-20 15:32:07 +0900

    spring 부분 돌아갈 수 있게 시큐리티 부분 없앰

--- back-end/src/main/java/org/finterest/security/account/mapper/UserDetailsMapper.java
+++
@@ -1,8 +0,0 @@
-package org.finterest.security.account.mapper;
-
-import org.finterest.security.account.domain.UserVO;
-
-
-public interface UserDetailsMapper {
-    public UserVO get(String username);
-}
