170d6c2c55c3351ddf369922feb0215e8adea3a5
minjeongj0 <160578786+minjeongj0@users.noreply.github.com>
2024-09-20 09:34:00 +0900

    feat: archive select all 기능 추가

--- front-end/src/views/Archive.vue
+++ front-end/src/views/Archive.vue
@@ -1,9 +1,28 @@
 <template>
   <div>
-    <H1>나는 학습</H1>
+    <h1>나는 학습</h1>
+    <div v-if="archives.length === 0">데이터를 불러오는 중...</div>
+    <ul v-else>
+      <li v-for="archive in archives" :key="archive.materialId">
+        <strong>{{ archive.title }}</strong> - {{ archive.description }}
+      </li>
+    </ul>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted } from "vue";
+import { useArchiveStore } from "@/store/archiveStore";
+
+const archiveStore = useArchiveStore();
+
+// 컴포넌트가 마운트될 때 데이터를 가져옵니다.
+onMounted(() => {
+  archiveStore.fetchArchive();
+});
+
+// archives를 컴포넌트에서 사용할 수 있도록 가져옵니다.
+const archives = archiveStore.archives;
+</script>
 
 <style scoped></style>
