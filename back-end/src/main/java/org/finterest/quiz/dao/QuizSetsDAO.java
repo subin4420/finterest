bcedd5e3151ee86f8b3494204d9b75fe369f4e7c
minjeongj0 <160578786+minjeongj0@users.noreply.github.com>
2024-09-24 15:27:34 +0900

    feat: 사용자 퀴즈 기능 추가

--- back-end/src/main/java/org/finterest/quiz/dao/QuizSetsDAO.java
+++ back-end/src/main/java/org/finterest/quiz/dao/QuizSetsDAO.java
@@ -1,6 +1,9 @@
 package org.finterest.quiz.dao;
 
-import org.finterest.quiz.domain.QuizSetsVO;
+import org.finterest.quiz.domain.vo.QuizResultVO;
+import org.finterest.quiz.domain.vo.QuizSetsVO;
+import org.finterest.quiz.domain.vo.QuizVO;
+import org.finterest.quiz.domain.vo.UserAnswerVO;
 import org.finterest.quiz.mapper.QuizSetsMapper;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +24,52 @@ public class QuizSetsDAO {
         return sqlSessionTemplate.getMapper(QuizSetsMapper.class).selectAllQuizSets();
     }
 
+    public List<QuizResultVO> selectQuizResultsByUserId(int userId){
+        return sqlSessionTemplate.getMapper(QuizSetsMapper.class).selectQuizResultsByUserId(userId);
+    }
+
     public List<QuizSetsVO> selectQuizSetsByCategory(int categoryId){
         return sqlSessionTemplate.getMapper(QuizSetsMapper.class).selectQuizSetsByCategory(categoryId);
     }
 
     public QuizSetsVO selectQuizSetById(int id){
         return sqlSessionTemplate.getMapper(QuizSetsMapper.class).selectQuizSetById(id);
+    }
+
+    public List<QuizVO> selectQuestionsBySetId(int setId){
+        return sqlSessionTemplate.getMapper(QuizSetsMapper.class).selectQuestionsBySetId(setId);
+    }
+
+    public QuizVO selectQuizById(int quizId){
+        return sqlSessionTemplate.getMapper(QuizSetsMapper.class).selectQuizById(quizId);
+    }
+
+    public int selectCorrectChoiceByQuizId(int quizId){
+        return sqlSessionTemplate.getMapper(QuizSetsMapper.class).selectCorrectChoiceByQuizId(quizId);
+    }
+
+    public int insertQuizResult(int userId, int setId, int totalScore, int maxScore) {
+        // MyBatis가 생성된 result_id를 keyProperty로 반환하도록 설정
+        sqlSessionTemplate.getMapper(QuizSetsMapper.class).insertQuizResult(userId, setId, totalScore, maxScore);
+
+        // 방금 삽입된 결과의 resultId를 반환
+        return sqlSessionTemplate.getMapper(QuizSetsMapper.class).getLastInsertedResultId();
+    }
+
+
+    public void insertUserAnswer(int resultId, int quizId, int userId, int userAnswer, boolean isCorrect){
+        sqlSessionTemplate.getMapper(QuizSetsMapper.class).insertUserAnswer(resultId, quizId, userId, userAnswer, isCorrect);
+    }
+
+    public int selectPointsForQuiz() {
+        return sqlSessionTemplate.getMapper(QuizSetsMapper.class).selectPointsForQuiz();
+    }
+
+    public QuizResultVO getQuizResult(int userId, int setId) {
+        return sqlSessionTemplate.getMapper(QuizSetsMapper.class).selectQuizResultByUserAndSet(userId, setId);
+    }
+
+    public List<UserAnswerVO> selectUserAnswers(int resultId, int userId){
+        return sqlSessionTemplate.getMapper(QuizSetsMapper.class).selectUserAnswers(resultId, userId);
     }
 }
