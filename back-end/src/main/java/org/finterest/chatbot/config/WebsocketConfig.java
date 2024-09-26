0bbebb31f4515ac84021ef88aa9c635c43af4024
subinPark <subin4420@gmail.com>
2024-09-26 17:40:16 +0900

    feat: 시큐리티 코드 우리 프로젝트에 맞춰서 일단 작성

--- back-end/src/main/java/org/finterest/chatbot/config/WebsocketConfig.java
+++ back-end/src/main/java/org/finterest/chatbot/config/WebsocketConfig.java
@@ -1,13 +1,19 @@
 package org.finterest.chatbot.config;
 
 import org.finterest.chatbot.websocket.ChatWebSocketHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.RequestUpgradeStrategy;
+import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocket
+@Profile("!test")
 public class WebsocketConfig implements WebSocketConfigurer {
     private final ChatWebSocketHandler chatWebSocketHandler;
 
@@ -20,5 +26,12 @@ public class WebsocketConfig implements WebSocketConfigurer {
         registry.addHandler(chatWebSocketHandler, "/ws/chat")
                 .setAllowedOrigins("*"); //CORS 설정
     }
-
+    @Bean
+    public DefaultHandshakeHandler handshakeHandler() {
+        return new DefaultHandshakeHandler(new TomcatRequestUpgradeStrategy());
+    }
+    @Bean
+    public RequestUpgradeStrategy requestUpgradeStrategy() {
+        return new TomcatRequestUpgradeStrategy(); // 톰캣을 사용하는 경우
+    }
 }
---
+++ back-end/src/main/java/org/finterest/security/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,25 @@
+package org.finterest.security.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.finterest.security.util.JsonResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component
+@Slf4j
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.error("========== 인가 에러 ============");
+        JsonResponse.sendError(response, HttpStatus.FORBIDDEN, "권한이 부족합니다.");
+    }
+}
