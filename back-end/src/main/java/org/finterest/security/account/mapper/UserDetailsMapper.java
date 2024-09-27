189685aa5614fbd4c333758d885041c0575f653e
이민규 <alsrb85822@gmail.com>
2024-09-27 21:41:12 +0900

    Delete back-end directory

--- back-end/src/main/java/org/finterest/security/account/mapper/UserDetailsMapper.java
+++
@@ -1,8 +0,0 @@
-package org.finterest.security.account.mapper;
-
-import org.finterest.security.account.domain.MemberVO;
-
-
-public interface UserDetailsMapper {
-    public MemberVO get(String username);
-}
