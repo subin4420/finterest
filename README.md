ab0efee8341215dd4f50875dc00cbe90437207fe
이민규 <alsrb85822@gmail.com>
2024-09-27 20:29:47 +0900

    add : (front)stock search, (back)board, conversion

--- README.md
+++
@@ -1,2 +0,0 @@
-# finterest
-KB IT's your life 5기 최종 프로젝트
--- front-end/src/stores/tradingService.js
+++
@@ -1,6 +0,0 @@
-import api from './api';
-
-export const getPortfolio = async () => {
-  const response = await api.get('/trading/portfolio');
-  return response.data;
-};
