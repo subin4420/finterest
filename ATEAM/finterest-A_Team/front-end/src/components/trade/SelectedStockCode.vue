3a888cef1d3e0599fbbfb55e622a444e288f0a78
M6F\student <alsrb85822@gmail.com>
2024-09-27 15:19:49 +0900

    add : (front)stock search, (back)board, conversion

---
+++ ATEAM/finterest-A_Team/front-end/src/components/trade/SelectedStockCode.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>선택한 주식 코드: {{ selectedStockCode }}</h2>
+  </div>
+</template>
+
+<script>
+import { useStockStore } from '@/stores/tradestock'; // Pinia 스토어 가져오기
+
+export default {
+  setup() {
+    const stockStore = useStockStore(); // Pinia 스토어 인스턴스 가져오기
+    return { selectedStockCode: stockStore.selectedStockCode }; // 선택한 주식 코드 가져오기
+  },
+};
+</script>
