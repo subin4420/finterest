83653e847019844ebc934eb14169e2f920b96594
minjeongj0 <160578786+minjeongj0@users.noreply.github.com>
2024-09-28 09:16:53 +0900

    Revert "feat: point 기능 수정"
    This reverts commit 53468d318026412e3b1a161de1e379c84c1ac55c.

--- front-end/src/stores/archiveStore.js
+++ front-end/src/stores/archiveStore.js
@@ -1,10 +1,8 @@
 import { reactive, toRefs } from 'vue';
-import { getArchive, getArchiveProgress } from '@/services/archiveService';
+import { getArchive } from '@/services/archiveService';
 
 const state = reactive({
-  archives: [], // 초기값 설정
-  inProgressArchives: [], // 진행 중인 학습 자료
-  completedArchives: [], // 완료된 학습 자료
+  archives: [] // 초기값 설정
 });
 
 const fetchArchive = async (type) => { // type 매개변수 추가
@@ -36,38 +34,6 @@ const fetchArchivesByCategory = async (categoryId) => {
   }
 };
 
-// 전체 학습 진행 상태 조회
-const fetchProgressArchives = async () => {
-  try {
-    const data = await getArchiveProgress();
-    state.archives = data.progress; // data.archives 대신 data.progress로 수정
-  } catch (error) {
-    console.error('Error fetching progress Archives:', error);
-  }
-};
-
-
-// 완료된 자료만 조회
-const fetchCompletedArchives = async () => {
-  try {
-    const data = await getArchiveProgress({ status: 'completed' }); // 완료된 자료만 조회
-    state.completedArchives   = data.progress; // data.archives 대신 data.progress로 수정
-  } catch (error) {
-    console.error('Error fetching completed Archives:', error);
-  }
-};
-
-// 미완료된 자료만 조회
-const fetchInProgressArchives = async () => {
-  try {
-    const data = await getArchiveProgress({ status: 'incomplete' }); // 미완료된 자료만 조회
-    state.inProgressArchives  = data.progress; // data.archives 대신 data.progress로 수정
-  } catch (error) {
-    console.error('Error fetching in-progress Archives:', error);
-  }
-};
-
-
 // 즐겨찾기한 자료만 조회
 const fetchFavoriteArchives = async () => {
   try {
@@ -84,9 +50,6 @@ export const useArchiveStore = () => {
     fetchArchive,
     fetchAllArchives,
     fetchArchivesByCategory,
-    fetchProgressArchives,
-    fetchCompletedArchives,
-    fetchInProgressArchives,
     fetchFavoriteArchives
   };
 };
