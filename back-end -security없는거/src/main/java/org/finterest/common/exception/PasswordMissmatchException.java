87ba59ec8a8a7dacd3da79d4532534e7159cca1d
subinPark <subin4420@gmail.com>
2024-10-07 17:37:52 +0900

    del: 시큐리티 없는 버전 삭제

--- back-end -security없는거/src/main/java/org/finterest/common/exception/PasswordMissmatchException.java
+++
@@ -1,7 +0,0 @@
-package org.finterest.common.exception;
-
-public class PasswordMissmatchException extends RuntimeException{
-    public PasswordMissmatchException() {
-        super("비밀번호가 일치하지 않습니다.");
-    }
-}
