4e048734f2dcc3d58319787fc47a8bee8a397064
minjeongj0 <160578786+minjeongj0@users.noreply.github.com>
2024-10-10 17:40:25 +0900

    feat: 백엔드 허용 도메인 추가

--- back-end/src/main/java/org/finterest/common/config/WebConfig.java
+++ back-end/src/main/java/org/finterest/common/config/WebConfig.java
@@ -73,7 +73,7 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:5173")  // 특정 도메인 허용
+                .allowedOrigins("http://localhost:5173", "hgkttp://localhost:5174")  // 특정 도메인 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메소드 지정
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true);  // 인증 정보 허용
